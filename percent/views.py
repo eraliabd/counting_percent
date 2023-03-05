b914a7d1efa80a62bfe0156c691b9939013e2ca5
eraliabdinazarov <eraliabdinazarov22@gmail.com>
2023-03-05 22:14:29 +0500

    commit and push percent files

--- percent/views.py
+++ percent/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
-def index(request):
-    return render(request, 'index.html', {"message": "Counting percent of number"})
+def response(context):
+    return render(context, 'index.html')
+
+
+async def index(request):
+    if request.method == 'POST':
+        number = int(request.POST.get('number', ''))
+        percent = int(request.POST.get('percent', ''))
+
+        percent_value = (number * percent) / 100
+        residual_value = number - percent_value
+        value_increase = number + percent_value
+
+        context = {
+            "percent_value": percent_value,
+            "residual_value": residual_value,
+            "value_increase": value_increase,
+            "percent": percent
+        }
+        return render(request, 'index.html', context)
+    else:
+        return render(request, 'index.html')
+    # return HttpResponse("successfully")
+
+
