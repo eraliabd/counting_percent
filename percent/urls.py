b317677eac0fb8c3ed20a080143947ab77f879c0
eraliabdinazarov <eraliabdinazarov22@gmail.com>
2023-03-07 00:29:19 +0500

    commit and push percent files

--- percent/urls.py
+++ percent/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, home, converter
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', home, name='home'),
+    path('percent/', index, name='percent'),
+    path('convert/', converter, name='convert'),
 ]
--- percent/views.py
+++ percent/views.py
@@ -1,4 +1,16 @@
+import os.path
+import uuid
+
+import win32com.client
+import comtypes.client
+
+from django.http import HttpResponse
 from django.shortcuts import render
+from docx2pdf import convert
+
+
+def home(request):
+    return render(request, 'home.html')
 
 
 def index(request):
@@ -20,3 +32,26 @@ def index(request):
         return render(request, 'index.html', context)
     else:
         return render(request, 'index.html')
+
+
+def converter(request):
+    if request.method == 'POST':
+        file_input = request.FILES.get('docs', '')
+        print("input file:", file_input)
+        pdf = f"pdf_{str(uuid.uuid4()).split('-')[-1]}.pdf"
+        file_output = os.path.abspath(f"files/pdf_files/{pdf}")
+        print("file output:", file_output)
+
+        word = win32com.client.Dispatch('Word.Application')
+        doc = word.Documents.Open(file_input)
+        print("Doc: ", doc)
+        doc.SaveAs(file_output, FileFormat=17)
+        doc.Close()
+        word.Quit()
+        with open(os.path.abspath(f"files/pdf_files/{pdf}"), 'rb') as file:
+            file = file.read()
+
+    # if str(input_doc).endswith(".doc"):
+            return render(request, 'doc_to_pdf.html', {"pdf_file": file})
+    else:
+        return render(request, 'doc_to_pdf.html')
